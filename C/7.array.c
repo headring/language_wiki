88707954aab80f51ad36320b983c2be771abc0be
SangHyun Kim <myhakiki@gmail.com>
2025-10-22 08:44:06 +0000

    feat: C언어 배열 내용 추가

--- C/7.array.c
+++ C/7.array.c
@@ -12,7 +12,7 @@ int main(void){
   strcpy(str1, "something"); // strcpy는 string copy의 약자이며 string.h 안에 존재
 
 
-  // 2차원 배열
+  /** 2차원 배열  */ 
   // 자료형 배열이름[세로(열)길이][가로(행)길이];
   int array2[3][3] = {
     {1, 2, 3},
@@ -20,6 +20,14 @@ int main(void){
     {7, 8, 9},
   };
   
+  // 문자열 배열 = char 배열이름[문자열 개수][각 문자열 당 문자의 개수 + 1]
+  char fourTrigrams[4][7] = { "Heaven", "Earth", "Water", "Fire" };
+  // 첫번째 배열의[0]은 "H" 이런식으로 저장
+  // 'H', 'e', 'a', 'v', 'e', 'n', '\0' -> 끝에는 항상 \0으로 저장
+
+  for (int i = 0; i < 4; i++){
+    printf("[%s]\n", fourTrigrams[i]);
+  }
  
   return 0;
 
